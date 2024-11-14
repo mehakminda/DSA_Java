c2632550d6900d3f32f0924a2c43b3c03ed95533
mehakminda <mehak_minda@yahoo.in>
2024-11-14 13:56:11 +0530

    Arrays2

---
+++ src/com/test/dsa/array/RemovedDuplicatesSortedArray5.java
@@ -0,0 +1,17 @@
+package com.test.dsa.array;
+
+/* Given a sorted array , remove the duplicate elements
+ * I/P a={2, 2, 2, 2, 2}
+ * O/P : {2}
+ * I/P arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5}
+ * O/P: {1, 2, 3, 4, 5}
+ * */
+
+public class RemovedDuplicatesSortedArray5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
