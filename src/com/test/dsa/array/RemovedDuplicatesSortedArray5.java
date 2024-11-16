7dd78f6f913a3458c6b76d437e634f54bca55a58
mehakminda <mehak_minda@yahoo.in>
2024-11-16 17:46:44 +0530

    Arrays

--- src/com/test/dsa/array/RemovedDuplicatesSortedArray5.java
+++ src/com/test/dsa/array/RemovedDuplicatesSortedArray5.java
@@ -11,7 +11,60 @@ public class RemovedDuplicatesSortedArray5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a[]={2, 2, 2, 2, 3};  
+		removeDuplicates_Method1(a);// **** we pass in the reference of array a ****
+		System.out.println("After Removing Duplicates Method1");
+		for(int i = 0; i < a.length; i++)
+	    {
+			System.out.print(a[i]+" ");
+	    }
+		System.out.println();
+		
 
+		
+		int a1[]={2, 2, 2, 4, 5};  
+		removeDuplicates_Method2(a1);// **** we pass in the reference of array a ****
+		System.out.println("After Removing Duplicates Method 2");
+		for(int i = 0; i < a1.length; i++)
+	    {
+			System.out.print(a1[i]+" ");
+	    }
+		System.out.println();
+
+	}
+	
+	//O(n) time and O(n) space
+	public static void removeDuplicates_Method1(int arr[]) {
+		int temp[]=new int[arr.length];
+		temp[0]=arr[0];
+		int res=1;
+		for(int i=0;i<arr.length;i++) {
+			if(temp[res-1]!=arr[i]) {
+				temp[res]=arr[i];
+				res++;
+			}
+		}
+		//copying contents back to arr
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=temp[i];
+		}
+	}
+	
+	//O(n) time and O(1) space
+	public static void removeDuplicates_Method2(int arr[]) {
+		int res=1;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[res-1]!=arr[i]) {
+				arr[res]=arr[i];
+				res++;
+			}
+			
+		}
+		//make rest of element as 0
+		for(int i=res;i<arr.length;i++) {
+			arr[i]=0;
+		}
+			
 	}
 
 }
