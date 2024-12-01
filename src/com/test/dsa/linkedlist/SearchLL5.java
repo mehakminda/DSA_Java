026f93c4f03734fdc002b64c33427454ea4191ce
mehakminda <mehak_minda@yahoo.in>
2024-12-02 00:12:08 +0530

    LinkedList2

--- src/com/test/dsa/linkedlist/SearchLL5.java
+++ src/com/test/dsa/linkedlist/SearchLL5.java
@@ -4,7 +4,65 @@ public class SearchLL5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		Node head=new Node(10);
+		Node temp1=new Node(30);
+		Node temp2=new Node(20);
+		Node temp3=new Node(40);
+		head.next=temp1;
+		temp1.next=temp2; //head.next.next=temp2;
+		temp2.next=temp3;
+		
+		System.out.println("Create a linked list");
+		ImplementationLL1.traversal_SLL_Iterative(head);
+		System.out.println();
+		
+		
+		int position;
+		position=SearchLLIterative(head,10);
+		System.out.println("Search 10 in  a linked list: "+ position);
+		position=SearchLLIterative(head,40);
+		System.out.println("Search 40 in  a linked list: "+ position);
+		position=SearchLLIterative(head,50);
+		System.out.println("Search 50 in  a linked list: "+ position);
+		
+		
+		System.out.println("");
+		System.out.println("------------------------------------------------------------------------------------");
+		System.out.println("");
+		
+		position=SearchLLRecursive(head,10,1);
+		System.out.println("Search 10 in  a linked list: "+ position);
+		position=SearchLLRecursive(head,40,1);
+		System.out.println("Search 40 in  a linked list: "+ position);
+		position=SearchLLRecursive(head,50,1);
+		System.out.println("Search 50 in  a linked list: "+ position);
 
+
+
+
+	}
+	public static int SearchLLIterative(Node head, int x) {
+		int pos=1;
+		while(head!=null) {
+			if(head.data==x) {
+				return pos;
+			}
+			else {
+				head=head.next;
+				pos++;
+			}
+		}
+		return -1;
+	}
+	
+	public static int SearchLLRecursive(Node head, int x , int pos) {
+		if(head==null) return -1;
+		if(head.data==x) return pos;
+		else return SearchLLRecursive(head.next,x,pos+1);
+		
 	}
 
 }
