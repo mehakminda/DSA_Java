192c98277b0e38298de29d2e0cc60ac0f5ee5873
mehakminda <mehak_minda@yahoo.in>
2025-05-22 11:02:06 +0530

    Adding programmingpatshala code as well.

--- src/com/test/dsa/array/MaximumChunks.java
+++ src/com/test/dsa/array/MaximumChunks.java
@@ -16,21 +16,35 @@ public class MaximumChunks {
 		// TODO Auto-generated method stub
 		
 		//int arr[]= {1,2,0,4,3,5};
-		int arr[]= {1,0,2,3,4};
+		//int arr[]= {1,0,2,3,4};
+		//int arr[] = {4,3,2,1,0};
+		int arr[]= {2,0,1,3};
 		int i=0;
+		int j=0;
 		int maxchunk=0;
 		while (i< arr.length) {
-			for(int j=i;j<arr.length;j++) {
-				if(canbeChunked(i,j,arr)) maxchunk++;
+			for(j=i;j<arr.length;j++) {
+				if(canbeChunked(i,j,arr)) {
+					maxchunk++;
+					break;
+				}
 			}
-			i++;
+			i=j+1;
 		}
 		System.out.println("max chunks is "+maxchunk);
 
 	}
-	public static boolean canbeChunked(int i, int j, int arr[] ){
-		if(arr[j]==i) return true;
-		else return false;
+	public static boolean canbeChunked(int i, int j,int arr[] ){
+		int count=0;
+		for(int k=i;k<=j;k++) {
+			if(arr[k]>=i && arr[k]<=j) {
+				count ++;
+			}
+		}
+			if(count ==(j-i+1)) return true;
+			else return false;
+			
+
 	}
 
 }
---
+++ src/com/test/dsa/array/renaissance/PreCompuatationTechniques.java
@@ -0,0 +1,28 @@
+package com.test.dsa.array.renaissance;
+
+
+/**
+ * IP: arr=[5,-1,2,4,10,2,7,1,8]
+ * 
+ * Given array find the sum of subarrays indexing between L and R
+ * IP: L,R = (0,5) (1,3) (3,6)  -> 3 queries (Q)
+ * 
+ * Solution: Prefix-Sum
+ * Time complexity: O(N+Q)
+ * Space Complexity: O(1)/O(N)
+ * 
+ * Given an array find the max of array from 0 to till an index 'idx'.
+ * 
+ * Solution: Prefix-Max
+ * Time complexity: O(N+Q)
+ * Space Complexity: O(1)/O(N)
+ * 
+ */
+public class PreCompuatationTechniques {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
